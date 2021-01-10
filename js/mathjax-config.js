f059420449fba3a24646c5bc1d768e3c6a679b83
George Litos <gl@cyberpunk.gr>
2021-01-10 14:08:42 +0200

    Build website

--- js/mathjax-config.js
+++ js/mathjax-config.js
@@ -1,10 +1,22 @@
+// MathJax Configuration
+//
+// v2 to v3 upgrade notes:
+// - The CommonHTML.linebreaks option is not yet implemented (but may be in a future release)
+// - The TeX.noUndefined.attributes option is not yet implemented (but may be in a future release)
 window.MathJax = {
-  CommonHTML: {linebreaks: {automatic: true}},
-  tex2jax: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']],
-    displayMath: [['$$', '$$'], ['\\[', '\\]']],
-    processEscapes: false
+  tex: {
+    inlineMath: [
+      ['$', '$'],
+      ['\\(', '\\)'],
+    ],
+    displayMath: [
+      ['$$', '$$'],
+      ['\\[', '\\]'],
+    ],
+    processEscapes: false,
+    packages: {'[+]': ['noerrors']},
   },
-  TeX: {noUndefined: {attributes: {mathcolor: 'red', mathbackground: '#FFEEEE', mathsize: '90%'}}},
-  messageStyle: 'none'
+  loader: {
+    load: ['[tex]/noerrors'],
+  },
 };
